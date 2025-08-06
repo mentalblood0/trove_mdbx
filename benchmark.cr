09d5f2294c7d2748d2e27a76c93116c52f8f492e
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-08-06 23:06:40 +0300

    benchmark

--- benchmark.cr
+++ benchmark.cr
@@ -1,24 +1,29 @@
 require "benchmark"
 
-require "./src/trove.cr"
+require "./src/trove_mdbx.cr"
 require "./spec/common.cr"
 
 chest = Trove::Chest.from_yaml <<-YAML
 env:
-  opts:
-    sophia:
-      path: /tmp/trove
-    db:
-      d: &ddbs
-        compression: zstd
-        compaction:
-          cache: 2_000_000_000
-      i:
-        *ddbs
-      u:
-        *ddbs
-      o:
-        *ddbs
+  path: /tmp/trove_mdbx
+  mode: 0o664
+  flags:
+    - MDBX_NOSUBDIR
+    - MDBX_LIFORECLAIM
+  mode: 0o664
+  db_flags:
+    d:
+      - MDBX_DB_DEFAULTS
+      - MDBX_CREATE
+    i:
+      - MDBX_DB_DEFAULTS
+      - MDBX_CREATE
+    u:
+      - MDBX_DB_DEFAULTS
+      - MDBX_CREATE
+    o:
+      - MDBX_DB_DEFAULTS
+      - MDBX_CREATE
 YAML
 
 cs = JSON.parse COMPLEX_STRUCTURE.to_json
@@ -27,41 +32,41 @@ v = "This is six levels deep"
 
 Benchmark.ips do |b|
   b.report "set+delete" do
-    chest.delete chest << cs
+    chest.transaction { |tx| tx.delete tx << cs }
   end
 end
 
-i = chest << cs
-chest.env.checkpoint
+i = Bytes.new 0
+chest.transaction { |tx| i = tx << cs }
 Benchmark.ips do |b|
   b.report "has key" do
-    raise "Can not get" unless chest.has_key? i, k
+    chest.transaction { |tx| raise "Can not get" unless tx.has_key? i, k }
   end
   b.report "has key (only simple)" do
-    raise "Can not get" unless chest.has_key! i, k
+    chest.transaction { |tx| raise "Can not get" unless tx.has_key! i, k }
   end
   b.report "get full" do
-    raise "Can not get" if chest.get(i) != cs
+    chest.transaction { |tx| raise "Can not get" if tx.get(i) != cs }
   end
   b.report "get field" do
-    raise "Can not get" if chest.get(i, k) != v
+    chest.transaction { |tx| raise "Can not get" if tx.get(i, k) != v }
   end
   b.report "get field (only simple)" do
-    raise "Can not get" if chest.get!(i, k) != v
+    chest.transaction { |tx| raise "Can not get" if tx.get!(i, k) != v }
   end
 end
 Benchmark.ips do |b|
   n = 10**4 - 1
-  (1..n).each { chest << cs }
+  (1..n).each { chest.transaction { |tx| tx << cs } }
   b.report "get one oid from index (unique)" do
-    chest.unique k, v
+    chest.transaction { |tx| tx.unique k, v }
   end
   b.report "get one oid from index" do
-    chest.where(k, v) { |ii| break }
+    chest.transaction { |tx| tx.where(k, v) { |ii| break } }
   end
   b.report "get #{n + 1} oids from index" do
     g = 0
-    chest.where(k, v) { |ii| g += 1 }
+    chest.transaction { |tx| tx.where(k, v) { |ii| g += 1 } }
     raise "#{g} != #{n + 1}" if g != n + 1
   end
 end
